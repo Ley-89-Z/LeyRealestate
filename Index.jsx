05b7e21bedb66e11eeb142faf1f69f67c26a251b
Ley-89-Z <laila_zarouki@outlook.com>
2025-04-01 17:45:57 +0200

    Delete Index.jsx

--- Index.jsx
+++
@@ -1,6 +0,0 @@
-git init  
-git add .  
-git commit -m "Eerste versie van vastgoedplatform"  
-git branch -M main  
-git remote add origin https://github.com/jouw-gebruikersnaam/real-estate-saas.git  
-git push -u origin main
