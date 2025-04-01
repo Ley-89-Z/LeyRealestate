8e57b3979972c0d1484949dea67b861ac9309498
Ley-89-Z <laila_zarouki@outlook.com>
2025-04-01 17:29:03 +0200

    Create Index.jsx

---
+++ Index.jsx
@@ -0,0 +1,6 @@
+git init  
+git add .  
+git commit -m "Eerste versie van vastgoedplatform"  
+git branch -M main  
+git remote add origin https://github.com/jouw-gebruikersnaam/real-estate-saas.git  
+git push -u origin main
